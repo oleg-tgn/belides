a78707e699c6e583040f961509bb67891adc325b
oleg-tgn <stelmah.taganrog@gmail.com>
2018-12-31 22:06:13 +0300

    add header add products

---
+++ js/script_slider.js
@@ -0,0 +1,12 @@
+// $('#single-slide-popular, #single-slide-rose, #single-slide-mixed, #single-slide-basket, #single-slide-hat, #single-slide-presents').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+// });
+
+$('.single-slide').slick({
+  infinite: false,
+  slidesToShow: 4,
+  dots: true,
+  slidesToScroll: 4,
+});
