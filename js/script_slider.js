a09e99b09fc9c5f9c26ede3d2a65d1d860761fba
oleg-tgn <stelmah.taganrog@gmail.com>
2019-01-10 18:43:32 +0300

    add adaptive styles

--- js/script_slider.js
+++ js/script_slider.js
@@ -13,4 +13,23 @@ $('.single-slide')
       slidesToShow: 4,
       dots: true,
       slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1023.99,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,            
+          }
+        },
+        {
+          breakpoint: 767.99,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,            
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
